e760f3518893860b112b74fa78ac74ca7c33e2bd
mj <47093544+aesodapop@users.noreply.github.com>
2025-12-08 12:59:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 about = st.Page("pages/about.py", title="About", icon=":material/info:", default=True)
-gallery = st.Page("pages/gallery.py", title="Gallery", icon=":material/images:")
+gallery = st.Page("pages/gallery.py", title="Gallery", icon=":material/photo:")
 
 pg = st.navigation({"Cheval": [about, gallery]})
 
