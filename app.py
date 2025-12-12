e2211664a4eccd8149b38654b66230d0c2eea280
mj <47093544+aesodapop@users.noreply.github.com>
2025-12-12 12:03:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ about = st.Page("pages/about.py", title="About", icon=":material/info:", default
 gallery = st.Page("pages/gallery.py", title="Gallery", icon=":material/photo:")
 map = st.Page("pages/map.py", title="Map", icon=":material/map:")
 
-pg = st.navigation({"Cheval": [about, gallery]})
+pg = st.navigation({"Cheval": [about, gallery, map]})
 
 pg.run()
